6dc842edccc03fc5122425b6a961b0fc74ba8883
Marnin-A <msmaudu@gmail.com>
2023-12-29 12:38:39 +0100

    fix: Incorrect rounded corners on ListPropertyCard

--- src/components/landing_page/listPropertyCard.tsx
+++ src/components/landing_page/listPropertyCard.tsx
@@ -4,8 +4,8 @@ export default function ListPropertyCard() {
 	return (
 		<section className="px-5 py-8 sm:px-16">
 			<div className="flex max-md:flex-col">
-				<div className="min-h-[400px] listProperty-bg rounded-t-2xl md:w-1/2 md:rounded-l-2xl"></div>
-				<div className="bg-primary-dark-green px-16 text-white flex flex-col justify-center gap-4 rounded-b-2xl max-md:p-8 max-md:items-center md:w-1/2 md:rounded-r-2xl">
+				<div className="min-h-[400px] listProperty-bg max-md:rounded-t-2xl md:w-1/2 md:rounded-l-2xl"></div>
+				<div className="bg-primary-dark-green px-16 text-white flex flex-col justify-center gap-4 max-md:rounded-b-2xl max-md:p-8 max-md:items-center md:w-1/2 md:rounded-r-2xl">
 					<h2 className="text-5xl font-bold">List your property with us</h2>
 					<p className="text-off-white">
 						Space sharing earns you extra income and opens up new opportunities
