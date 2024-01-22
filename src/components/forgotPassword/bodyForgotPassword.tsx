787894c7f5e262afd57649cbe47d7eb0d8f09bb4
Marnin-A <msmaudu@gmail.com>
2024-01-22 19:41:40 +0100

    fix: fix 4 for useSearchParams() should be wrapped in a suspense boundary

--- src/components/forgotPassword/bodyForgotPassword.tsx
+++ src/components/forgotPassword/bodyForgotPassword.tsx
@@ -1,39 +1,16 @@
 "use client";
 import React from "react";
-import ForgotPasswordCard from "./forgotPasswordCard";
-import ForgotPasswordSuccess from "./forgotPasswordSuccess";
-import PasswordOtpCard from "./passwordOtpCard";
-import ManageSearchParams from "@/hooks/updateSearchParams";
-import NewPasswordCard from "./newPasswordCard";
-import ResetPasswordSuccess from "./resetPasswordSuccess";
+import dynamic from "next/dynamic";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function BodyForgotPassword() {
+	const DynamicComponentRender = dynamic(() => import("./renderComponent"), {
+		ssr: false,
+		loading: () => <CircularProgress color="success" className="m-auto" />,
+	});
 	return (
-		<React.Suspense>
-			<div className="flex-1 flex flex-col items-center justify-center w-full overflow-y-scroll max-sm:items-start">
-				<RenderComponents />
-			</div>
-		</React.Suspense>
-	);
-}
-
-function RenderComponents() {
-	const { getURLParam } = ManageSearchParams();
-	const view = getURLParam("view");
-	console.log(view);
-	return (
-		<>
-			{view === "forgotPasswordSuccess" ? (
-				<ForgotPasswordSuccess />
-			) : view === "enterOTP" ? (
-				<PasswordOtpCard />
-			) : view === "newPassword" ? (
-				<NewPasswordCard />
-			) : view === "resetPasswordSuccess" ? (
-				<ResetPasswordSuccess />
-			) : (
-				<ForgotPasswordCard />
-			)}
-		</>
+		<div className="flex-1 flex flex-col items-center justify-center w-full overflow-y-scroll max-sm:items-start">
+			<DynamicComponentRender />
+		</div>
 	);
 }
