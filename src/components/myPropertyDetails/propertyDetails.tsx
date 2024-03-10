5764789e4bac5b42b3858f02a3d7dca77a2b0128
Marnin-A <msmaudu@gmail.com>
2024-03-10 08:23:33 +0100

    fix: correct similar component structure

--- src/components/myPropertyDetails/propertyDetails.tsx
+++ src/components/myPropertyDetails/propertyDetails.tsx
@@ -1,12 +1,61 @@
+"use client";
 import React from "react";
 import SideBar from "../propertyDetails/sideBar";
 import PropertyDetailsBody from "./propertyDetailsBody";
+import { useGetPropertyDetailsQuery } from "@/app/redux/services/apiServices";
+import LoadingSpinner from "../loadingSpinner";
+import { useDispatch } from "react-redux";
+import { setDialogOpen } from "@/app/redux/slices/dialogSlice";
 
-export default async function PropertyDetails() {
-	return (
-		<div className="flex">
-			<SideBar />
-			<PropertyDetailsBody />
+export default function PropertyDetails() {
+	const alertId = React.useId();
+	const dispatch = useDispatch();
+	const [propertyId, setPropertyId] = React.useState("");
+	const { data, isError, error } = useGetPropertyDetailsQuery({
+		propertyId: propertyId,
+	});
+	React.useEffect(() => {
+		setPropertyId(
+			window.location.pathname.split("/")[
+				window.location.pathname.split("/").length - 1
+			]
+		);
+		if (isError) {
+			dispatch(
+				setDialogOpen({
+					alertId: alertId,
+					message: "",
+					open: true,
+					title: "Sorry an error occurred",
+				})
+			);
+		}
+	}, [alertId, dispatch, error, isError]);
+
+	return data && "status" in data && data.status === "success" ? (
+		<div className="flex max-lg:flex-col">
+			<SideBar
+				accommodationType={data.data.accomodation.accommodationType}
+				address={data.data.accomodation.address}
+				city={data.data.accomodation.city}
+				hostingPeriodFrom={data.data.accomodation.hostingPeriodFrom}
+				hostingPeriodTo={data.data.accomodation.hostingPeriodTo}
+				reason={data.data.accomodation.whyListing}
+				state={data.data.accomodation.state}
+				key={propertyId + "-side-bar"}
+			/>
+			<PropertyDetailsBody
+				accommodationName={data.data.accomodation.accommodationName}
+				accommodationRules={data.data.accomodation.accommodationRules}
+				availability={data.data.accomodation.status}
+				description={data.data.accomodation.description}
+				images={data.data.accomodation.gallery}
+				price={data.data.accomodation.price}
+				userId={data.data.accomodation.createdBy}
+				key={data.data.accomodation.accommodationName}
+			/>
 		</div>
+	) : (
+		<LoadingSpinner className="m-auto" />
 	);
 }
--- src/components/myPropertyDetails/propertyDetailsBody.tsx
+++ src/components/myPropertyDetails/propertyDetailsBody.tsx
@@ -1,51 +1,64 @@
-import {
-	Money,
-	RadioButtonChecked,
-	RoomSharp,
-	Rule,
-} from "@mui/icons-material";
+import { Money, RadioButtonChecked, Rule } from "@mui/icons-material";
 import Image from "next/image";
 import React from "react";
 import AboutComponent from "../propertyDetails/aboutComponent";
 import AccommodationRules from "../propertyDetails/accommodationRules";
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/redux/store";
 
-export default function PropertyDetailsBody() {
+import Link from "next/link";
+import LoadingSpinner from "../loadingSpinner";
+type props = {
+	accommodationName: string;
+	description: string;
+	availability: "available" | "not available" | undefined;
+	price: string;
+	images: Array<{ imageId: string; imageUrl: string }>;
+	accommodationRules: Array<string>;
+	userId: string;
+};
+
+export default function PropertyDetailsBody(props: props) {
+	const hostDetails = useSelector((state: RootState) => state.HostDetails);
+	React.useEffect(() => {
+		hostDetails;
+	}, [hostDetails]);
+
 	return (
 		<div className="flex flex-col items-center">
 			<div className="flex items-center gap-5 mb-5 flex-wrap">
 				<button className="flex items-center gap-[10px] py-[22px] px-[30px] bg-off-white">
 					<RadioButtonChecked htmlColor="#03796E" />
-					Available
+					{props.availability === "available" ? "Available" : "Not Available"}
 				</button>
 				<button className="flex items-center gap-[10px] py-[22px] px-[30px] bg-off-white whitespace-nowrap">
 					<Money htmlColor="#03796E" />
-					NGN 65,000 per night
+					NGN {props.price.toLocaleString()} per night
 				</button>
-				<button className="flex items-center gap-[10px] py-[22px] px-[30px] bg-off-white">
-					<RoomSharp htmlColor="#03796E" />
-					Location
-				</button>
-				<button className="flex items-center gap-[10px] py-[22px] px-[30px] bg-off-white whitespace-nowrap">
+				<Link
+					href={"#accommodationRules"}
+					className="flex items-center gap-[10px] py-[22px] px-[30px] bg-off-white whitespace-nowrap"
+				>
 					<Rule htmlColor="#03796E" />
 					Accommodation rules
-				</button>
+				</Link>
 			</div>
-			<div className="flex gap-5 h-[500px] w-full relative">
-				<div className="flex items-center justify-center min-h-full w-[450px] overflow-hidden">
+			<div className="flex aspect-auto gap-5 w-max relative mb-16 max-md:w-full">
+				<div className="flex items-center justify-center min-h-full w-[450px] overflow-hidden max-md:w-7/12">
 					<Image
-						src="/Mobile-logo.svg"
+						src={props.images[0].imageUrl}
 						alt="Mobile Squazzle Logo"
 						width={530}
 						height={582}
 						placeholder="empty"
 						priority={false}
-						className="w-full h-auto m-auto"
+						className="w-full h-auto"
 					/>
 				</div>
-				<div className="flex flex-col items-center justify-between">
-					<div className="flex items-center justify-center min-h-1/2 w-[300px] overflow-hidden">
+				<div className="flex flex-col items-center justify-between max-md:w-5/12">
+					<div className="flex items-center justify-center min-h-1/2 w-[250px] overflow-hidden max-md:w-4/5">
 						<Image
-							src="/Mobile-logo.svg"
+							src={props.images[1].imageUrl}
 							alt="Mobile Squazzle Logo"
 							width={350}
 							height={255}
@@ -54,9 +67,9 @@ export default function PropertyDetailsBody() {
 							className="w-full h-auto m-auto"
 						/>
 					</div>
-					<div className="flex items-center justify-center min-h-1/2 w-[300px] overflow-hidden">
+					<div className="flex items-center justify-center min-h-1/2 w-[250px] overflow-hidden max-md:w-4/5">
 						<Image
-							src="/Mobile-logo.svg"
+							src={props.images[2].imageUrl}
 							alt="Mobile Squazzle Logo"
 							width={530}
 							height={582}
@@ -66,19 +79,30 @@ export default function PropertyDetailsBody() {
 						/>
 					</div>
 				</div>
-				<div className="absolute px-4 py-[7px] bg-white opacity-95 bottom-4 right-4">
-					+17 Photos
+				<div className="absolute px-2 py-[4px] bg-white opacity-95 bottom-4 right-4">
+					{props.images.length > 3
+						? `+${props.images.length - 3} Photos`
+						: null}
 				</div>
 			</div>
 			<div className="max-w-[800px] ">
-				<h1 className="text-[54px] font-semibold mb-10">Primrose View</h1>
-				<AboutComponent
-					aboutText={
-						"From the outside this house looks nice and traditional. It has windows that let in plenty of light. The house is equipped with a small kitchen and two bathrooms, it also has a cozy living room, two bedrooms, a roomy dining area, a playroom and a cozy garage.The building is fairly rounded in shape.The roof is low and v-shaped and is covered with grey ceramic tiles. The house itself is surrounded by a tranquil garden, with beautiful primrose flowers and various rock formations"
-					}
-				/>
+				<div className="flex items-center gap-10 mb-10">
+					<h1 className="text-[54px] font-semibold max-md:text-2xl">
+						{props.accommodationName}
+					</h1>
+					<Image
+						src={hostDetails.hostImg}
+						alt={"Profile picture of the host"}
+						width={56}
+						height={56}
+						placeholder="empty"
+						priority={false}
+						className="min-w-[56px] h-auto rounded-full"
+					/>
+				</div>
+				<AboutComponent aboutText={props.description} />
 				<hr className="border-primary-mid-grey my-10" />
-				<AccommodationRules />
+				<AccommodationRules rules={props.accommodationRules} />
 			</div>
 		</div>
 	);
