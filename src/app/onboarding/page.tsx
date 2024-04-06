672a7ac5ea7c420692ae564e2c6d331581b6ee33
Marnin-A <msmaudu@gmail.com>
2024-04-06 15:06:58 +0100

    style: make onboarding page mobile responsive

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -3,29 +3,29 @@ import React from "react";
 
 export default function Page() {
 	return (
-		<div className="onboarding-bg flex flex-col justify-center items-center px-12 py-16	">
-			<div className="flex flex-col gap-6 justify-center items-center text-off-white mb-20">
-				<h1 className="text-[54px]  font-semibold">
+		<div className="onboarding-bg flex flex-col justify-center items-center px-12 py-16	text-center">
+			<div className="flex flex-col gap-6 justify-center items-center text-off-white md:mb-20 mb-6">
+				<h1 className="md:text-[54px] md:font-semibold font-bold text-4xl leading-relaxed">
 					Find that perfect home with Squazzle.
 				</h1>
-				<p className="text-3xl">
+				<p className="md:text-3xl text-xl">
 					Get the best accommodation and list your property for the right
 					prices.
 				</p>
-				<h3 className="text-3xl font-semibold">
+				<h3 className="md:text-3xl font-semibold text-xl">
 					So what would you like to do?
 				</h3>
 			</div>
-			<div className="flex items-center justify-center gap-16 w-full">
+			<div className="flex items-center flex-wrap justify-center md:gap-16 gap-6 w-full">
 				<Link
 					href={"/manageAccount/myListing/editProperty?view=overview"}
-					className="w-max py-5 px-24 bg-primary-lightgreen text-center text-primary-dark-green text-xl font-bold rounded-2xl"
+					className="w-max md:py-5 md:px-24 py-4 px-8 bg-primary-lightgreen text-center text-primary-dark-green md:text-xl text-lg font-bold rounded-2xl"
 				>
 					List accommodation
 				</Link>
 				<Link
 					href="/accommodations"
-					className="w-max py-5 px-24 bg-primary-lightgreen text-center text-primary-dark-green text-xl font-bold rounded-2xl"
+					className="w-max md:py-5 md:px-24 py-4 px-6 bg-primary-lightgreen text-center text-primary-dark-green md:text-xl text-lg font-bold rounded-2xl"
 				>
 					Browse accommodation
 				</Link>
